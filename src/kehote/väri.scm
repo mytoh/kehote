7d7c0c9fa0cd65fa459759d1641a7921490bb544
mytoh <mytoh@gmail.com>
2012-10-03 14:25:30 +0900

    change to use kirjasto.vari for colour-string function

--- src/kehote/väri.scm
+++ src/kehote/väri.scm
@@ -1,18 +1,4 @@
 (define-module kehote.väri
-  (export colour-string)
-  (use srfi-13))
+  (extend kirjasto.väri))
 (select-module kehote.väri)
 
-(define (colour-string colour-number s)
-  (let ((shell (sys-basename (sys-getenv "SHELL"))))
-    (cond
-      ((string=? shell "tcsh")
-       (string-concatenate
-         `("[38;5;" ,(number->string colour-number) "m"
-           ,s
-           "[0m")))
-      (else
-        (string-concatenate
-          `("[38;5;" ,(number->string colour-number) "m"
-            ,s
-            "[0m"))))))
